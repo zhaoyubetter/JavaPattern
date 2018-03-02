843733ada76a46700e04aa1c0cf7f10af95f3aea
zhaoyu57 <zhaoyu1@jd.com>
2018-03-02 19:12:46 +0800

    添加部分代码

--- src/main/java/com/better/groovy/forandroid/StringFinder.groovy
+++ src/main/java/com/better/groovy/forandroid/StringFinder.groovy
@@ -22,6 +22,17 @@ class StringFinder {
     private boolean isStop
     private Map<String, String> moduleStringMap = new HashMap<>()
 
+    // 字符串资源目录
+    private final String string_res = "values%s"
+
+    // xml文件过滤器
+    private final xmlFileFilter = new FileFilter() {
+        @Override
+        boolean accept(File pathname) {
+            return pathname.name.endsWith(".xml")
+        }
+    }
+
     /**
      * 主方法一，获取模块内的 strings.xml
      * @param moduleDir 模块路径
@@ -36,15 +47,19 @@ class StringFinder {
             throw new RuntimeException("the Aodule ${dir} Directory is wrong!")
         }
 
-        File tappResDir = new File(appResDir)
-        this.appResDir = tappResDir;
-        if (!tappResDir.exists() || !tappResDir.isDirectory()) {
-            throw new RuntimeException("the App  ${dir} Res Directory is wrong!")
-        }
-        innerModuleStringXml(file)
+        // app 主工程资源目录，默认values
+//        File tappResDir = new File(appResDir)
+//        this.appResDir = tappResDir;
+//        if (!tappResDir.exists() || !tappResDir.isDirectory()) {
+//            throw new RuntimeException("the App  ${dir} Res Directory is wrong!")
+//        }
+//        innerModuleStringXml(file)
 
-        // 拿到模块中所有的 String key
-        moduleStringMap.each {it -> println(it.key + "  --> " + it.value)}
+        // 1.=== 拿到模块中所有的 String key
+        moduleStringMap.each { it -> println(it.key + "  --> " + it.value) }
+
+        // 2.=== 从主app/res中查找 ===
+        exportPartStringFromApp(moduleStringMap, appResDir)
     }
 
     private innerModuleStringXml(File file) {
@@ -77,9 +92,56 @@ class StringFinder {
         }
     }
 
-    // 测试
-    static void main(args) {
-        def moduleDir = "/Users/zhaoyu/Documents/github/KotlinAndroidDemo/widget/src/main/"
-        new StringFinder().exportModuleStringXml(moduleDir, moduleDir)
+    private def exportPartStringFromApp(Map<String, String> moduleStringMap, String appResFolder) {
+        File stringFolder = new File(appResFolder, String.format(string_res, ""))
+        LinkedList<File> allFiles = new LinkedList<>()      // 所有资源文计
+        LinkedList<File> allFolder = new LinkedList<>()     // 文件夹
+        stringFolder.eachFile { it ->
+            if (it.isDirectory())
+                allFolder.add(it)
+            else {
+                allFiles.add(it)
+            }
+        }
+        while (!allFolder.isEmpty()) {
+            File dir = allFolder.removeFirst()
+            dir.eachFile { it ->
+                if (it.isDirectory())
+                    allFolder.add(it)
+                else {
+                    allFiles.add(it)
+                }
+            }
+        }
+        // 遍历所有文件
+        defStringXmlFileParser(allFiles)
     }
+
+    private def defStringXmlFileParser(List<File> xmlResFiles) {
+        Map<String, String> stringMap = new HashMap<>()
+        xmlResFiles.each { it ->
+            def root = new XmlParser(false, false).parse(it.absolutePath)
+            // 闭包的递归， 先定义，再赋值
+            def eachNode
+            eachNode = { node ->
+                // 所有孩子-》节点-》遍历
+                node.children().each { child ->
+                    println(child)
+//                    if(child.attributes().containKey("string")) {
+//                        println(node.attributes()["string"])
+//                    }
+                }
+                node.value()?.each {eachNode.call(it)}
+            }
+            eachNode.call(root)
+        }
+    }
+
+// 测试
+    static void main(args) {
+        def moduleDir = "D:\\git.jd\\mae.aura.flowcenter\\aura-flowcenter"
+        def appResDir = "D:\\git.jd\\jdme_android\\app\\src\\main\\res"
+        new StringFinder().exportModuleStringXml(moduleDir, appResDir)
+    }
+
 }
