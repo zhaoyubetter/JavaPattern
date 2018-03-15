b8b4168af264a800ee2aa5528716ba30a3389aa5
zhaoyu57 <zhaoyu1@jd.com>
2018-03-15 18:55:27 +0800

    基本完成 脚本。

--- src/main/java/com/better/groovy/forandroid/resource/ResReName.groovy
+++ src/main/java/com/better/groovy/forandroid/resource/ResReName.groovy
@@ -23,6 +23,7 @@ import java.util.regex.Matcher
  e. menu:             R.menu.XXX
  f. raw:              R.raw.XXX
  g. xml:              R.xml.xxx
+
  2.values: string、string-array、attr、color、dimens、style
  a. string:            R.string.XXX      与 @string/XXX
  b. arrays:            R.array.XXX
@@ -30,26 +31,36 @@ import java.util.regex.Matcher
  d. color              R.color.XXX       与 @color/XXX
  e. dimens             R.dimen.XXX       与 @dimen/XXX
  f. style              R.style.XXX       与 @style/XXX
+
+ ** 步骤：**
+ 注意：所有资源替换来源为本工程资源来源，因为有些资源有可能是公共库中的，替换就麻烦了
+ 1. 先替换layout；
+
  </pre>
  */
 class ResReName {
 
     static final class DirNamePrefixFilter implements FilenameFilter {
         def prefix = ""
+
         DirNamePrefixFilter(String prefix) {
             this.prefix = prefix
         }
+
         @Override
         boolean accept(File dir, String name) {
             return dir.isDirectory() && name.startsWith(prefix)
         }
     }
 
-
-
     // java 源文件 such as  R.string.aaabb  R . string. aa
-    def JAVA_REGX = ~/R((\s*?)\.(\s*?)string(\s*?)\.(\s*?)(\w+))?/  // R.string  R.anim
-    def XML_REGX = ~/(@string\/(.+?))"/                             // @+id/string.xxxx
+    def JAVA_REGX = ~/(R(\s*?)\.(\s*?)string(\s*?)\.(\s*?))(\w+)/  // R.string  R.anim
+    def XML_REGX = ~/(@string\/(w+))"/                             // @+id/string.xxxx
+
+    // 新资源前缀
+    def static final NEW_FREFIX = "_flowcenter_"
+    // 老的
+    def static final OLD_FREFIX = "_"
 
     // 源代码路径
     File srcDir
@@ -62,6 +73,8 @@ class ResReName {
     }
 
     def renameLayout() {
+        println("----- 修改layout资源开始")
+
         Set<String> layoutNameSet = new HashSet<>()
         // 1.获取所有layout开头的文件夹
         File[] layoutDirs = resDir.listFiles(new Tools.DirNamePrefixFilter("layout"))
@@ -72,54 +85,59 @@ class ResReName {
             }
         }
 
-        def java_regx = ~/R((\s*?)\.(\s*?)layout(\s*?)\.(\s*?)(\w+))?/
-        def xml_regx = ~/(@layout\/(.+?))"/
+        def java_regx = ~/(R(\s*?)\.(\s*?)layout(\s*?)\.(\s*?))(\w+)/
+        def xml_regx = ~/(@layout\/)(\w+)"/
 
-        replaceSrcDir(srcDir, layoutNameSet, java_regx)
+        // 修改 java 源代码部分的 R.layout.XXX，有些哥们变态这样写 R. layout.  XXX 都是支持的
+        //replaceSrcDir(srcDir, layoutNameSet, java_regx)
+        // 修改资源文件中，资源文件不会出现空格现象
+        //LayoutRename.replaceResDir(resDir, layoutNameSet, xml_regx)
+        LayoutRename.renameLayoutFile(resDir)
+        println("----- 修改layout资源结束")
     }
 
-    private void replaceSrcDir(File file, set, java_regx) {
+    // ====== 源代码中的部分  start =============================================
+    private void replaceSrcDir(File file, Set<String> set, java_regx) {
         if (file.exists()) {
             if (file.isDirectory()) {
-                file.eachFile { it -> replaceSrcDir(file, set, java_regx) }
+                file.eachFile { it -> replaceSrcDir(it, set, java_regx) }
             } else {
-                handleSrcFile(file, set, java_regx)
+                if (file.name.endsWith(".java") || file.name.endsWith(".kt")) {  // 只处理Java与Kt文件
+                    handleSrcFile(file, set, java_regx)
+                }
             }
         }
     }
 
     private void handleSrcFile(File file, Set<String> set, regex) {
-        RandomAccessFile raf = new RandomAccessFile(file, "rw")
-        long lastPoint = 0
-        raf.each { line ->
-            Matcher matcher = line =~ regex
-            while (matcher.find()) {
-                // R.ooo.XXX 需要转变成 to R.ooo.前缀_ooo
-                String layoutName = matcher.group(6)
-
+        String fileContent = file.getText()     // 文件都是文本文件
+        StringBuffer sb = new StringBuffer()             // 结果内容
+        Matcher matcher = fileContent =~ regex
+        while (matcher.find()) {
+            String oldResName = matcher.group(6)
+            if (set.contains(oldResName)) {
+                String newResName = NEW_FREFIX + oldResName
+                if (oldResName.startsWith(OLD_FREFIX)) {
+                    newResName = NEW_FREFIX + oldResName.substring(OLD_FREFIX.length())
+                }
+                matcher.appendReplacement(sb, "\$1$newResName") // 拼接 保留$1分组,替换$6分组
+            } else {
+                matcher.find()              // 继续下一次查找，避免死循环
             }
-            lastPoint = raf.getFilePointer()
+        }
+        // 修改了文件时，才写入文件
+        if (sb.length() > 0) {
+            matcher.appendTail(sb)              // 添加结尾
+            file.write(sb.toString())           // 写回文件
         }
     }
-
-    private void replaceDir2() {
-
-    }
+    // ====== 源代码中的部分  end =============================================
 
     // 测试
     static void main(args) {
-        // 正则匹配java文件内的string
-        def JAVA_REGX = ~/R((\s*?)\.(\s*?)string(\s*?)\.(\s*?)(\w+))?/  // R.string.aaabb  R . string. aa
-        // 正则匹配xml文件内的string
-        def XML_REGX = ~/(@string\/(.+?))"/             // @+id/string.xxxx
-
-        String line = "String str = getString(  R  .   string  .   me_loadinbbb);" +
-                "        String str = getString(  R  .   string  .   me_loadinga);" +
-                "        String str = getString( R.string.me_loadidfsfnga);"
-        Matcher matcher = line =~ JAVA_REGX
-
-        while (matcher.find()) {
-            println(matcher.group(6))
-        }
+        def src = ""
+        def res = ""
+        ResReName resReName = new ResReName(src, res)
+        resReName.renameLayout()
     }
 }
