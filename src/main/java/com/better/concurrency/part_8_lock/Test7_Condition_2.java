02db7fff62382768cf3682fb3c873d5b77f8eb8a
zhaoyu57 <zhaoyu1@jd.com>
2018-08-30 22:10:29 +0800

    一个condition

--- src/main/java/com/better/concurrency/part_8_lock/Test7_Condition_2.java
+++ src/main/java/com/better/concurrency/part_8_lock/Test7_Condition_2.java
@@ -8,14 +8,16 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 交替输出 一个Condition
+ */
 public class Test7_Condition_2 {
 
     private static final String[] units_olds = {"壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾"};
     private static final String[] units_now = {"一", "二", "三", "四", "五", "六", "七", "八", "九", "十"};
 
     private static final Lock lock = new ReentrantLock();
-    private static Condition condition_old = lock.newCondition();
-    private static Condition condition_new = lock.newCondition();
+    private static Condition condition = lock.newCondition();
 
     public static void main(String[] args) throws InterruptedException {
         //   for (int i = 0; i < units_now.length; i++) {
@@ -25,24 +27,34 @@ public class Test7_Condition_2 {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                printlnOld(0);
+                while (true) {
+                    for (int i = 0; i < units_now.length; i++) {
+                        printlnOld(i);
+                    }
+                }
             }
         }).start();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                println_now(0);
+                while (true) {
+                    for (int i = 0; i < units_now.length; i++) {
+                        println_now(i);
+                    }
+                }
             }
         }).start();
     }
 
-    private static void printlnOld(final int index)  {
+    private static void printlnOld(final int index) {
         try {
             lock.lock();
+//            Utils.println("old");
             Thread.sleep(new Random().nextInt(1000));
             Utils.println(units_olds[index]);
-            condition_old.signalAll();
+            condition.signal();   // 完成工作，唤醒其他，进入休眠，等待唤醒
+            condition.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -52,11 +64,16 @@ public class Test7_Condition_2 {
 
     private static void println_now(int index) {
         try {
+            lock.lock();
+//            Utils.println("now");
             Thread.sleep(new Random().nextInt(2000));
+            Utils.println(units_now[index]);
+            condition.signal();
+            condition.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
-
-
 }
