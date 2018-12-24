b869fe210191cdb0fe85859209380991f81cddd2
zhaoyubetter <zhaoyubetter@126.com>
2018-12-24 11:04:54 +0800

    add ui

--- src/main/kotlin/tools/img/ui/MainFrame.kt
+++ src/main/kotlin/tools/img/ui/MainFrame.kt
@@ -1,7 +1,11 @@
 package tools.img.ui
 
-import java.awt.EventQueue
-import javax.swing.JFrame
+import com.sun.codemodel.internal.fmt.JTextFile
+import org.fusesource.jansi.Ansi
+import org.intellij.lang.annotations.Flow
+import java.awt.*
+import javax.swing.*
+
 
 /**
  * 主窗口
@@ -26,6 +30,49 @@ class MainFrame(title: String) : JFrame() {
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         setSize(800, 600)
         setLocationRelativeTo(null)
+
+        // 分为3大块
+        val borderLayout = BorderLayout()             // 布局管理器
+        val mainPanel = JPanel(borderLayout)
+
+        // add to mainPanel
+        mainPanel.add(getTopPanel(), BorderLayout.NORTH)
+        mainPanel.add(getCenterPanel(), BorderLayout.CENTER)
+        mainPanel.add(getBottomPanel(), BorderLayout.SOUTH)
+
+        this.add(mainPanel)
     }
 
+    private inline fun getTopPanel(): JPanel {
+
+        return JPanel().apply {
+            layout = GridLayout(2, 1, 0, 0)
+            add(JPanel().apply {
+                layout = FlowLayout(FlowLayout.LEFT)
+                add(JLabel("来源目录："))
+                add(JTextField(42))
+                add(JButton("..."))
+                add(JCheckBox("含子"))
+            })
+
+            add(JPanel().apply {
+                layout = FlowLayout(FlowLayout.LEFT)
+                add(JLabel("目标目录："))
+                add(JTextField(42))
+                add(JButton("..."))
+            })
+        }
+    }
+
+    private inline fun getCenterPanel(): JPanel {
+        val centerPanel = JPanel()
+        centerPanel.background = Color.CYAN
+        return centerPanel
+    }
+
+    private inline fun getBottomPanel(): JPanel {
+        val bottomPanel = JPanel()
+        bottomPanel.background = Color.yellow
+        return bottomPanel
+    }
 }
