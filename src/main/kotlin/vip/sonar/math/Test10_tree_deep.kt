18f457a5a7e33198d56264f1f3fb5ab0add2f872
better <zhaoyubetter@gmail.com>
2019-01-13 23:20:02 +0800

    update

---
+++ src/main/kotlin/vip/sonar/math/Test10_tree_deep.kt
@@ -0,0 +1,55 @@
+package vip.sonar.math
+
+fun main(args: Array<String>) {
+    buildPreTree(Pair("boy", "男生"))
+    buildPreTree(Pair("book", "篮球"))
+    buildPreTree(Pair("basket", "篮子"))
+    buildPreTree(Pair("back", "往后"))
+    buildPreTree(Pair("bad", "坏的"))
+    buildPreTree(Pair("buy", "买"))
+
+    println(search("basket"))
+}
+
+// 根
+private var parent = TreeNode('0')
+
+// 构造前缀树
+private fun buildPreTree(word: Pair<String, String>) {
+    var parent: TreeNode = parent
+    var pre = ""          // 前部分
+
+    word.first.forEachIndexed { index, c ->
+        parent = if (parent.sons.contains(c)) {
+            parent.sons[c]!!
+        } else {
+            val son = TreeNode(c, prefix = pre, explain = "")
+            parent.sons[c] = son
+            son
+        }
+        pre += c
+
+        if (index == word.first.length - 1) {   // 写入注释
+            parent.explain = word.second
+        }
+    }
+}
+
+private fun search(word: String): String {
+    var parent = parent
+    word.forEachIndexed { i, c ->
+        if (parent.sons[c] == null) {
+            return "unKnown"
+        }
+        parent = parent.sons[c]!!
+    }
+    return parent.explain!!
+}
+
+
+private data class TreeNode(val label: Char,
+                            val sons: HashMap<Char, TreeNode> = HashMap(),
+                            var prefix: String? = null,
+                            var explain: String? = null)
+
+
