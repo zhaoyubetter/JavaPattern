19de85f14ad14cd0ff4caf1dd2624c8ac0de89a5
better <zhaoyubetter@gmail.com>
2019-04-12 00:10:39 +0800

    update 二叉树查找

--- src/main/kotlin/vip/sonar/suanfa/binarytree/SearchBinaryTree.kt
+++ src/main/kotlin/vip/sonar/suanfa/binarytree/SearchBinaryTree.kt
@@ -16,7 +16,7 @@ fun main() {
             var tmp: Node<T>? = root
 
             while (tmp != null) {
-                if (tmp?.data?.compareTo(data)!! < 0) { // 大走右子树
+                if (data > tmp.data) {
                     if (tmp.right == null) {
                         tmp.right = node
                         return
@@ -36,8 +36,8 @@ fun main() {
             var tmp: Node<T>? = this
             while (tmp != null) {
                 tmp = when {
-                    tmp?.data?.compareTo(data)!! < 0 -> tmp?.right
-                    tmp?.data?.compareTo(data)!! > 0 -> tmp?.left
+                    data > tmp?.data!! -> tmp?.right
+                    data < tmp?.data!! -> tmp?.left
                     else -> return tmp
                 }
             }
@@ -45,60 +45,81 @@ fun main() {
         }
 
         /**
-         * 删除节点
+         * 删除节点,复杂一点 // 分3种情况
          */
         fun deleteNode(data: T) {
-            var find: Node<T>? = this
-            var parent: Node<T>? = null  // 待删除节点的父节点
-            while (find != null && find.data != data) {
-                parent = find
-                if (find?.data?.compareTo(data)!! < 0) {
-                    find = find?.right
-                } else if (find?.data?.compareTo(data)!! > 0) {
-                    find = find?.left
+            var node: Node<T>? = this
+            var parent: Node<T>? = null
+
+            // 先查找
+            while (node != null && node?.data != data) {
+                parent = node
+                node = if (data > node?.data!!) {
+                    node?.right
+                } else {
+                    node?.left
                 }
             }
-            if (find == null) {
-                println("没有找到")
+
+
+            if (node == null) {
+                println("$data not found.")
                 return
             }
 
+            // 待删除的child
             var child: Node<T>? = null
+            // === 3种情况
+            node?.let {
+                if (it.left == null && it.right == null) {
+                    // 1.left与right都为null
+                    child = null
+                } else if (it.left != null || it.right != null) {
+                    // 2.left or right 有一个不为null
+                    child = if (it.left != null) it.left else it.right
+                } else {
+                    // 3. left与right都存在的情况，从右孩子对应的左子树种找到最小节点
+                    var min = it.right
+                    var minP = min
+                    while (min?.left != null) {
+                        min = min.left
+                    }
 
-            // == 删除分3种情况
-            if (find.left == null && find.right == null) {
-                // 1. 待删除节点没有子
-                child = null
-            } else if (find.left != null || find.right != null) {
-                // 2.有一个孩子（左 or 右）
-                child = if (find.left != null) find.left else find.right
-            } else {
-                // 3.有2个孩子,从右树中,一直找到最小叶子
-                var min = find.right    // 最小的
-                var minP: Node<T>? = min          // 父
-                while (min?.left != null) {
-                    minP = min
-                    min = min.left
+                    it.data = min?.data!!   // 直接改变值，完成替换
+                    node = min              // 下面就变成了删除 min 了
+                    parent = minP           // 改变父
                 }
-                find.data = min?.data!!  // 替换数据，交换值
-                find = min               // 删除改变，下面就变成了删除 find 了
-                parent = minP
             }
 
-            // === 删除操作
-            if(parent == null) {    // 删除根节点
-                println("删除了根节点")
-                this.left = null
-                this.right = null
-            } else if(parent.left == find) {
-                parent.left = child
+            // 删除操作
+            if (parent == null) {
+                println("删除的是根节点")
+            } else if (parent?.left == node) {     // child 为 null ，直接删除
+                parent?.left = child
             } else {
-                parent.right = child
+                parent?.right = child
             }
+        }
+
+        fun getMaxNode():Node<T> {
+            // 直接获取右直到叶子
+            var node = this
+            while(node.right != null) {
+                node = node.right!!
+            }
+            return node
+        }
+
+        fun getMinNode():Node<T> {
+            var node = this
+            while(node.left != null) {
+                node = node.left!!
+            }
+            return node
         }
     }
 
-    fun <T:Comparable<T>> inOrder(root:Node<T>?) {
+    fun <T : Comparable<T>> inOrder(root: Node<T>?) {
         root?.apply {
             inOrder(root.left)
             print("${root.data} ")
@@ -117,9 +138,14 @@ fun main() {
         insertNode(10, tree)
     }
 
+    println("search: ${tree.searchData(10)}")
+
 
     inOrder(tree)
-    tree.deleteNode(4)
+    tree.deleteNode(7)
     println()
     inOrder(tree)
+
+    println("min: ${tree.getMinNode().data}")
+    println("max: ${tree.getMaxNode().data}")
 }
