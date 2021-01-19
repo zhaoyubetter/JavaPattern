5abefe5dee933369792eadb2e1953c2dea3ab2cd
zhaoyubetter <zhaoyubetter@126.com>
2021-01-19 23:06:34 +0800

    AVL 树 打印

--- src/main/kotlin/vip/sonar/suanfa/abook/tree/AVLTreeTest.kt
+++ src/main/kotlin/vip/sonar/suanfa/abook/tree/AVLTreeTest.kt
@@ -20,7 +20,18 @@ import org.junit.Test
 class AVLTreeTest {
     @Test
     fun test() {
-
+        val avlTree = AVLTree<Int>().apply {
+            this.insert(2);
+            this.insert(3);
+            this.insert(4);
+            this.insert(5);
+            this.insert(6);
+            this.insert(7);
+            this.insert(15);
+            this.insert(16);
+            this.insert(14);
+        }
+        TreeBeauty.show(avlTree.root)
     }
 
     class AVLTree<T : Comparable<T>> {
@@ -73,6 +84,7 @@ class AVLTreeTest {
                     t = doubleRightAndLeft(t)
             }
 
+            // 更新该节点高度
             t?.height = Math.max(height(t?.left), height(t?.right)) + 1
             return t
         }
@@ -211,6 +223,80 @@ class AVLTreeTest {
             var left: AVLNode<T>? = null
             var right: AVLNode<T>? = null
             var height: Int = 0
+        }
+    }
+
+    class TreeBeauty {
+        companion object {
+            val map = mutableMapOf<Int, AVLTree.AVLNode<*>?>()
+
+            private fun <T : Comparable<T>> getDeep(root: AVLTree.AVLNode<T>?): Int {
+                return if (root == null) 0 else 1 + Math.max(getDeep(root.left), getDeep(root.right))
+//                return if (root == null) 0 else root.height
+            }
+
+            fun <T : Comparable<T>> writeArray(currNode: AVLTree.AVLNode<T>?, rowIndex: Int, colIndex: Int, res: Array<Array<String>>, treeDeep: Int) {
+                if (currNode == null) {
+                    return
+                }
+                map[currNode.data as Int] = currNode as? AVLTree.AVLNode<Int>
+                val currNode = currNode!!
+
+                // 先将当前节点保存到二维数组中
+                res[rowIndex][colIndex] = (currNode.data).toString()
+                // 计算当前位于树的第几层
+                val currLevel = (rowIndex + 1) / 2
+                // 若到了最后一层，则返回
+                if (currLevel == treeDeep) return
+                // 计算当前行到下一行，每个元素之间的间隔（下一行的列索引与当前元素的列索引之间的间隔）
+                val gap = treeDeep - currLevel - 1
+
+                // 对左儿子进行判断，若有左儿子，则记录相应的"/"与左儿子的值
+                if (currNode.left != null) {
+                    res[rowIndex + 1][colIndex - gap] = "/"
+                    writeArray(currNode.left, rowIndex + 2, colIndex - gap * 2, res, treeDeep)
+                }
+
+                // 对右儿子进行判断，若有右儿子，则记录相应的"\"与右儿子的值
+                if (currNode.right != null) {
+                    res[rowIndex + 1][colIndex + gap] = "\\"
+                    writeArray(currNode.right, rowIndex + 2, colIndex + gap * 2, res, treeDeep)
+                }
+            }
+
+            fun <T : Comparable<T>> show(root: AVLTree.AVLNode<T>?) {
+                root?.apply {
+                    val treeDeep = getDeep(root)
+                    // 最后一行的宽度为2的（n - 1）次方乘3，再加1
+                    // 作为整个二维数组的宽度
+                    val arrayHeight = treeDeep * 2 - 1
+                    val arrayWidth = (2 shl treeDeep - 2) * 3 + 1
+                    // 用一个字符串数组来存储每个位置应显示的元素
+                    val res = Array(arrayHeight) { Array(arrayWidth) { " " } }
+
+                    // 从根节点开始，递归处理整个树
+                    // res[0][(arrayWidth + 1)/ 2] = root.data.toString();
+                    writeArray(this, 0, arrayWidth / 2, res, treeDeep)
+
+                    // 此时，已经将所有需要显示的元素储存到了二维数组中，将其拼接并打印即可
+                    res.forEach { line ->
+                        line.forEach {
+                            var value = it
+                            try {
+                                // 判断是红还是黑哈
+                                val node = map[it.toInt()]
+                                node?.apply {
+                                    value = this.data.toString()
+                                }
+                            } catch (e: Exception) {
+
+                            }
+                            print(value)
+                        }
+                        println()
+                    }
+                }
+            }
         }
     }
 }
